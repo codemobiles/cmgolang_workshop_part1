99fbe3fe1a2ae32dfec391cba951841e6fb1722c
codemobiles <chaiyasit.t@gmail.com>
2020-07-27 21:35:23 +0700

    สอน Golang Ep25# Slice Remove Specific Index

--- demo12_slice_remove/index.go
+++ demo12_slice_remove/index.go
@@ -17,8 +17,18 @@ func main() {
    showSlice(numbers)
    numbers = numbers[0: len(numbers)-1]
    showSlice(numbers)
+
+   // remove specific index
+   numbers = removeIndex(numbers, 1)
+   showSlice(numbers)
+
 }
 
 func showSlice(x []int){
    fmt.Printf("len=%d cap=%d slice=%v\n",len(x),cap(x),x)
+}
+
+
+func removeIndex(s []int, index int) []int {
+	return append(s[:index], s[index+1:]...)
 }
