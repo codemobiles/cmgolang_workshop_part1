c4f73c1552acc0c01db9091c3d922efcf79751bc
codemobiles <chaiyasit.t@gmail.com>
2020-07-27 09:35:54 +0700

    สอน Golang Ep15# Define Function + Return

--- demo5/index.go
+++ demo5/index.go
@@ -8,6 +8,11 @@ func main() {
 	fn1();
 	fn2("Good Morning")
 	fn3("Good Morning", 1)
+	const a,b = 2,3
+	fmt.Printf("%d+%d=%d\n",a,b, sum(a,b))
+
+	x,y := swap(a,b)
+	fmt.Printf("%d,%d => %d,%d\n",a,b, x,y)
 }
 
 func fn1(){
@@ -21,6 +26,14 @@ func fn2(msg string){
 func fn3(title string, version int){
 	fmt.Print(title);
 	fmt.Println(version);
+}
+
+func sum(a int, b int) int{
+	return a + b;
+}
+
+func swap(a int, b int) (int, int){
+	return b,a
 }
 
 
