fb0b1a83bef26e0a3717425315b67ff8f4d0a720
codemobiles <chaiyasit.t@gmail.com>
2020-07-27 21:52:30 +0700

    สอน Golang Ep27# Make Map to Dynamic

--- demo13_map/index.go
+++ demo13_map/index.go
@@ -7,4 +7,11 @@ import (
 func main() {
    var numbers = map[string]int{"one":1, "two":2, "three":3}
    fmt.Println("",numbers["two"])
+
+   var colors = make(map[string]string)
+   colors["red"] = "#f00";
+   colors["green"] = "#0f0";
+   colors["blue"] = "#00f";
+   fmt.Println("",colors)
+   fmt.Println("",colors["green"])
 }
