32f44f1662e930ffcf08a695b903a1035a94dd57
codemobiles <chaiyasit.t@gmail.com>
2020-07-30 07:50:05 +0700

    สอน Golang Ep35# Interface and Polymorphism

---
+++ demo18_Interface/index.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type shape interface {
+	area() float64
+}
+
+type circle struct {
+	radius float64
+}
+
+type rectangle struct {
+	width, height float64
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (r rectangle) area() float64 {
+	return r.width * r.height
+}
+
+func getArea(s shape) float64{
+	return s.area()
+}
+
+func main() {
+	r := rectangle{width: 10, height: 10}
+	c := circle{radius: 10}
+
+	fmt.Printf("Rectangle: %f\n", getArea(r))
+	fmt.Printf("Circle: %f\n", getArea(c))
+}
